2a2e348f9b61d75a0c27aad151f7839f2f6a0ab0
fsantos1990 <majinho@msn.com>
2025-10-12 21:29:18 -0300

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  publicDir: 'public',   // garante que o Vite copie a pasta
-  base: '/',             // garante paths absolutos
-  build: { outDir: 'dist' }
-})
